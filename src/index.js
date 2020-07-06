faab1854af587b1be9fcd39093558b637eda3eb8
Matias Bouin <49533289+matiasbouin@users.noreply.github.com>
2020-07-06 00:09:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-//Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
 
-//It should display a h1 heading.
+const name = "Matias";
+const lname = "Bouin";
+const n = 7;
+
 ReactDOM.render(
   <div>
-    <h1>Hola BROS</h1>
-    <ul>
-      <li>No sigas leyendo</li>
-      <li>NO LEAS</li>
-      <li>puto el q lee</li>
-    </ul>
+    <h1>Hello {name} {lname}!</h1>
+    <p>your lucky number is {n}</p>
   </div>,
   document.getElementById("root")
 );
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
