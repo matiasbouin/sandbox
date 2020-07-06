8fb7ffead6bd74d09528880833e1fa507dc3e7c1
Matias Bouin <49533289+matiasbouin@users.noreply.github.com>
2020-07-06 14:38:23 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
+//Create a react app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
 
+//It should display 2 paragraph HTML elements.
 const name = "Matias";
-const lname = "Bouin";
-const n = 7;
-
+const currentYear = new Date();
+const year = currentYear.getFullYear();
 ReactDOM.render(
   <div>
-    <h1>Hello {name} {lname}!</h1>
-    <p>your lucky number is {n}</p>
+    <h1>Created by {name}</h1>
+    <h2>Copyright {year}</h2>
   </div>,
   document.getElementById("root")
 );
+
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
