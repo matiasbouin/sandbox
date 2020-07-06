e10eec5fda606e9cfd089568a01a2c104025de7f
Matias Bouin <49533289+matiasbouin@users.noreply.github.com>
2020-07-06 20:52:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-//Create a react app from scratch.
+//Create a React app from scratch.
 import React from "react";
 import ReactDOM from "react-dom";
 
-//It should display 2 paragraph HTML elements.
-const name = "Matias";
-const currentYear = new Date();
-const year = currentYear.getFullYear();
+const date = new Date();
+const currentHour = date.getHours();
+
+let greeting;
+
+const customColor = {
+  color: ""
+};
+
+if (currentHour < 12) {
+  greeting = "Good morning";
+  customColor.color = "red";
+} else if (currentHour > 12 && currentHour < 18) {
+  greeting = "Good afternoon";
+  customColor.color = "green";
+} else {
+  greeting = "Good evening";
+  customColor.color = "blue";
+}
+
 ReactDOM.render(
-  <div>
-    <h1>Created by {name}</h1>
-    <h2>Copyright {year}</h2>
-  </div>,
+  <h1 className="heading" style={customColor}>
+    {greeting}
+  </h1>,
   document.getElementById("root")
 );
-
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
